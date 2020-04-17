1ce4364054fdeb149928b54fe4748b9870b589f8
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-04-17 10:37:15 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-/*
- © Copyright 2020 Micro Focus or one of its affiliates.
- * The only warranties for products and services of Micro Focus and its affiliates and licensors
- * (“Micro Focus”) are set forth in the express warranty statements accompanying such products and
- * services. Nothing herein should be construed as constituting an additional warranty. Micro Focus
- * shall not be liable for technical or editorial errors or omissions contained herein. The informa-
- * tion contained herein is subject to change without notice. Contains Confidential Information.
- * Except as specifically indicated otherwise, a valid license is required for possession, use or
- * copying. Consistent with FAR 12.211 and 12.212, Commercial Computer Software, Computer Software
- * Documentation, and Technical Data for Commercial Items are licensed to the U.S. Government under
- * vendor's standard commercial license.
+﻿/*
+ © Copyright 2020 tapasadhikary.com or one of its affiliates.
+ * Some Sample Copyright Text Line
+ * Some Sample Copyright Text Line
+ * Some Sample Copyright Text Line
+ * Some Sample Copyright Text Line
+ * Some Sample Copyright Text Line
+ * Some Sample Copyright Text Line
 */
 
 const test = () => {
