1d025437a50cd7b524cf79cc0c348906ca7f81bd
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-10-28 23:24:35 +0530

    Update addcopyright.sh

--- addcopyright.sh
+++ addcopyright.sh
@@ -1,4 +1,7 @@
-#!/bin/bash  
+#!/bin/bash
+
+## Solution found as part of this stackoverflow discussion: https://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
+
 for x in $*; do  
 head -$COPYRIGHTLEN $x | diff copyright.txt - || ( ( cat copyright.txt; echo; cat $x) > /tmp/file;  
 mv /tmp/file $x )  
