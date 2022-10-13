5245f076df299ed02a87dd9072eb30c7ebfafc0a
giulio-Joshi <me@joshi.it>
2022-10-13 21:59:47 +0200

    Added mimetype protection
    * Added DEBUG env variable to check script internals&commands
    
    * Validated method with `shellcheck` utility, improving from `for`+`find`
    
    * added zipfile in `/drill/` as example of file not to be touched

--- addcopyright.sh
+++ addcopyright.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-## Solution found as part of this stackoverflow discussion: https://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
+if [ -n "${DEBUG}" ];
+then
+    echo "Debug Mode, display commands & dry-run (no actual modifications to files)."
+    set -x
+fi
 
-for x in $*; do  
-head -$COPYRIGHTLEN $x | diff copyright.txt - || ( ( cat copyright.txt; echo; cat $x) > /tmp/file;  
-mv /tmp/file $x )  
+if [ -z "${1}" ] || [ -d "${1}" ];
+then
+    WORKDIR="${1}"
+else
+    WORKDIR="."
+fi 
+
+
+find "${WORKDIR}" -type f -not -path "./.git/*" -print0 | while IFS= read -r -d '' file_name
+do
+    isTextFile=$(file --no-buffer -i "${file_name}"  )
+    echo " Found ${file_name} which has ${isTextFile} "
+    if [[ "${isTextFile}" =~ ".* text/plain;.*" ]];
+    then
+        echo "${file_name} skipped (not text),"
+        continue;
+    fi
+
+    if [ -n "${DEBUG}" ];
+    then
+        echo "Skip ${file_name}, debug is dry-run mode."
+        continue;
+    fi
+
+    ## Solution found as part of this stackoverflow discussion: https://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
+    head "-${COPYRIGHTLEN}" "${file_name}" | diff copyright.txt - || ( ( cat copyright.txt; echo; cat "${file_name}") > /tmp/file;  \
+        mv /tmp/file "${file_name}" )  
 done 
+
+
